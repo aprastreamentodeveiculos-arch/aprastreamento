6b3843f609ff8ea62ef8fe4d63653c40fb64c14a
AP Rastreamento <aprastreamentodeveiculos@gmail.com>
2026-07-14 16:44:47 -0300

    chore: remove endpoint temporario de inject-admin

--- backend/src/modules/usuarios/usuario.routes.ts
+++ backend/src/modules/usuarios/usuario.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import * as usuarioController from './usuario.controller';
-import { injectAdminEndpoint } from './inject.controller';
 
 const router = Router();
 
-router.post('/inject-admin', injectAdminEndpoint);
 router.get('/', usuarioController.listUsuarios);
 router.post('/', usuarioController.createUsuario);
 router.put('/:id', usuarioController.updateUsuario);
